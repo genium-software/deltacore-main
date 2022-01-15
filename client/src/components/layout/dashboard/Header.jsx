d805044b648a443453c1046760e0b07f88003eae
Jie Fan <jfan0010@student.monash.edu>
2022-01-16 00:30:24 +1100

    fixed children width larger than parent
    with default css, this component width is longer than parents which will cause website scrolling horizontally

--- client/src/components/layout/dashboard/Header.jsx
+++ client/src/components/layout/dashboard/Header.jsx
@@ -5,7 +5,6 @@ import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -97,7 +96,6 @@ export default function MiniDrawer({ children }) {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
       <AppBar
         color={"inherit"}
         position="fixed"

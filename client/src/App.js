253d39048b51bd5ac95c101be944fd572a6cdb88
Jie Fan <jfan0010@student.monash.edu>
2022-01-16 00:09:02 +1100

    removed redux logo + added mui base css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import { ThemeProvider } from '@material-ui/styles'; // eslint-disable-line import/no-extraneous-dependencies
 import { createTheme } from '@material-ui/core/styles';
 import { theme as colour } from './components/theme/default.js';
 import { HashRouter as Router } from "react-router-dom";
 import AuthRouter from "./router/AuthRouter";
+import { CssBaseline } from '@material-ui/core';
 
 const App = () => {
 
@@ -22,11 +22,15 @@ const App = () => {
   });
 
   return (
+
     <Router>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <AuthRouter />
       </ThemeProvider>
     </Router>
+
+
   );
 }
 

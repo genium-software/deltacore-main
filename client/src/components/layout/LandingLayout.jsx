a3fa8607b4f60b78e91b0b3884f57c6130c3496b
Jie Fan <jfan0010@student.monash.edu>
2022-01-18 19:10:28 +1100

    import bootstrap css to fix landing page header

--- client/src/components/layout/LandingLayout.jsx
+++ client/src/components/layout/LandingLayout.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import LandingHeader from "./landing-page/LandingHeader";
 import Footer from "./Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LandingLayout = ({ url, children }) => {
   console.log("MATCH:", url);
